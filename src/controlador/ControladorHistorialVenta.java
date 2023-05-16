101a4975e4a710a2aa6d472a4d82c890d1fa9340
Hookedy <80292951+Hookedy@users.noreply.github.com>
2023-05-16 11:11:28 -0500

    Update ControladorHistorialVenta.java

--- src/controlador/ControladorHistorialVenta.java
+++ src/controlador/ControladorHistorialVenta.java
@@ -1,68 +1,33 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controlador;
 
-import modelo.dao.CategoriaDAO;
-import modelo.dao.ClienteDAO;
-import modelo.dao.PedidoDAO;
-import modelo.dao.ProductoDAO;
-import modelo.dao.UsuarioDAO;
+import modelo.dao.*;
 import vista.PanelHistorialVentas;
 
-/**
- *
- * @author krypt97
- */
 public class ControladorHistorialVenta {
 
-    // ATRIBUTOS DE CLASE
     private PanelHistorialVentas miPanelHistorialVentas;
-    private CategoriaDAO miCategoriaDAO;
-    private ProductoDAO miProductoDAO;
-    private UsuarioDAO miUsuarioDAO;
-    private ClienteDAO miClienteDAO;
-    private PedidoDAO miPedidoDAO;
+    private DAOFactory daoFactory = DAOFactory.getInstancia();
     private int idVentaSeleccionado;
     private String dniUsuarioSelecionado;
     private String dniClienteSeleccionado;
 
-    // ENLACE VISTA
     public void setPanelHistorialVentas(PanelHistorialVentas miPanelHistorialVentas) {
         this.miPanelHistorialVentas = miPanelHistorialVentas;
     }
 
-    // Dao
-    public void setUsuarioDAO(UsuarioDAO miUsuarioDAO) {
-        this.miUsuarioDAO = miUsuarioDAO;
-    }
-
-    public void setProductoDAO(ProductoDAO miProductoDAO) {
-        this.miProductoDAO = miProductoDAO;
-    }
-
-    public void setClienteDAO(ClienteDAO miClienteDAO) {
-        this.miClienteDAO = miClienteDAO;
-    }
-
-    public void setPedidoDAO(PedidoDAO miPedidoDAO) {
-        this.miPedidoDAO = miPedidoDAO;
-    }
-
-    public void setCategoriaDAO(CategoriaDAO miCategoriaDAO) {
-        this.miCategoriaDAO = miCategoriaDAO;
-    }
-
-    // MÉTODOS DE CLASE
     public void cargarPedidosSeleccionados() {
-        idVentaSeleccionado = miPanelHistorialVentas.idVentaSeleccionado();
-        dniUsuarioSelecionado = miPanelHistorialVentas.dniUsuarioSeleccionado();
-        dniClienteSeleccionado = miPanelHistorialVentas.dniClienteSeleccionado();
-        miPanelHistorialVentas.desempaquetarDatosUsuario(miUsuarioDAO.buscarUsuario(dniUsuarioSelecionado));
-        miPanelHistorialVentas.desempaquetarDatosCliente(miClienteDAO.buscarCliente(dniClienteSeleccionado));
-        miPanelHistorialVentas.SetTablaPedidos(miPedidoDAO.listarCarritoPedidos(idVentaSeleccionado), miProductoDAO.listarProductos(), miCategoriaDAO.listarCategorias());
-        miPanelHistorialVentas.tblPedidos.requestFocus();
+        idVentaSeleccionado = miPanelHistorialVentas.getIdVentaSeleccionado();
+        dniUsuarioSelecionado = miPanelHistorialVentas.getDniUsuarioSeleccionado();
+        dniClienteSeleccionado = miPanelHistorialVentas.getDniClienteSeleccionado();
+        UsuarioDAO usuarioDAO = daoFactory.getUsuarioDAO();
+        ClienteDAO clienteDAO = daoFactory.getClienteDAO();
+        PedidoDAO pedidoDAO = daoFactory.getPedidoDAO();
+        ProductoDAO productoDAO = daoFactory.getProductoDAO();
+        CategoriaDAO categoriaDAO = daoFactory.getCategoriaDAO();
+
+        miPanelHistorialVentas.desempaquetarDatosUsuario(usuarioDAO.buscarUsuario(dniUsuarioSelecionado));
+        miPanelHistorialVentas.desempaquetarDatosCliente(clienteDAO.buscarCliente(dniClienteSeleccionado));
+        miPanelHistorialVentas.setTablaPedidos(pedidoDAO.listarCarritoPedidos(idVentaSeleccionado), productoDAO.listarProductos(), categoriaDAO.listarCategorias());
+        miPanelHistorialVentas.requestFocusOnPedidosTable();
     }
 }
